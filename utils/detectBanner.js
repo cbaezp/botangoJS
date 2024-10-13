485303af995b52b0ec68803b32a6762ae3b14727
Cbz <91438023+cbaezp@users.noreply.github.com>
2024-10-13 16:17:04 -0700

    Create detectBanner.js

---
+++ utils/detectBanner.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+//
+export const getBannerArea = async () => {
+    const url = 'http://127.0.0.1:8000/get_banner_area';
+    const data = {
+        "threshold": 0.5,
+        "draw": false,
+        "dpi": 96,
+        "banner_width": 1200,
+        "banner_height": 300
+    };
+
+    try {
+        const response = await axios.post(url, data);
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching banner area:', error);
+        throw error;
+    }
+};
